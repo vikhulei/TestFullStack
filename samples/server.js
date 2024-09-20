22c54928dd6e2abebe42c5f0538aea9d7262b7c7
vikhulei <vikhulei@gmail.com>
2024-09-20 19:54:24 +0100

    router created

--- samples/server.js
+++ samples/server.js
@@ -3,3 +3,13 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+
+app.use(express.json())
+
+// app.get("/", (req, res) => res.send("I am in the root"))
+
+app.post("/", (req, res) => {
+    const { name } = req.body
+    res.send(`The name is ${name}`)
+    console.log(name)
+})
--- server.js
+++ server.js
@@ -3,3 +3,15 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+
+app.use(express.json())
+
+app.use("/api/users", require('./api/users'))
+
+// app.get("/", (req, res) => res.send("I am in the root"))
+
+// app.post("/", (req, res) => {
+//     const { name } = req.body
+//     res.send(`The name is ${name}`)
+//     console.log(name)
+// })
