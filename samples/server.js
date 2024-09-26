f3888c99d14849232370d0689189a00b4c0e8092
vikhulei <65869915+vikhulei@users.noreply.github.com>
2024-09-26 15:21:39 +0100

    sample formed

--- samples/server.js
+++ samples/server.js
@@ -1,12 +1,20 @@
 const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000
+const connectDB = require('./config/db')
 
-app.use(express.json())
+const app = express ()
 
-app.use("/api/users", require("./api.users"))
+connectDB()
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+//Initialize middleWare
+app.use(express.json({ extended: false}))
 
+const PORT = process.env.PORT || 5000
 
+app.get('/', (req, res) => res.send('API running'))
 
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/post', require('./routes/api/post'))
+
+app.listen(PORT, () => console.log(`server started on ${PORT}`))
