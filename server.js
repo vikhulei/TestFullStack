901d90bd458d7602e0eb308fda6c772c781c24ad
vikhulei <vikhulei@gmail.com>
2024-09-27 20:48:24 +0100

    practicing

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+connectDB()
 
 app.use(express.json())
 app.use("/routes/api/users", "./api/users")
@@ -10,12 +11,4 @@ app.use("/routes/api/auth", "./api/auth")
 app.use("/routes/api/profile", "./api/profile")
 app.use("/routes/api/post", "./api/post")
 
-app.use("/api/users", require('./api/users'))
-
-// app.get("/", (req, res) => res.send("I am in the root"))
-
-// app.post("/", (req, res) => {
-//     const { name } = req.body
-//     res.send(`The name is ${name}`)
-//     console.log(name)
-// })
+app.listen(PORT, () => console.log(`Server on the port ${PORT}`))
